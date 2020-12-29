6dc63986e0c6ad016bead3a3b62554262d79e11b
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2020-12-29 12:48:10 +0900

    chore: insert sample data

---
+++ jdbc-bad-sample/Update.sql
@@ -0,0 +1,1 @@
+UPDATE TABLE_A SET address='address1' WHERE id=1;
--- jdbc-bad-sample/src/main/resources/db/migration/V1.0.0__create.sql
+++ jdbc-bad-sample/src/main/resources/db/migration/V1.0.0__create.sql
@@ -8,3 +8,6 @@ CREATE TABLE TABLE_B (
     name VARCHAR(30),
     address VARCHAR (256)
 );
+
+INSERT INTO TABLE_A (id, name, address) VALUES (1, 'name1', null);
+INSERT INTO TABLE_B (name, address) VALUES ('name1', 'address1');
